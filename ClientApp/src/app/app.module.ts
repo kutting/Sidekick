c3e5e71b77c12777f4874aff2d56b3d42b08c0b9
KDU <ken@goruck.com>
2020-01-27 22:01:55 -0500

    added comics & vendors modules

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -14,21 +13,27 @@ import { AppRoutingModule } from './app-routing.module';
 // The app page components
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+// The app modules
+import { ComicsModule } from './comics/comics.module';
+import { VendorsModule } from './vendors/vendors.module';
+
 @NgModule({
-  declarations: [
-	AppComponent,
-	NavMenuComponent,
-	FetchDataComponent,
-   ],
-  imports: [
-	BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-	HttpClientModule,
-	FormsModule,
-	MaterialModule,
-	BrowserAnimationsModule,
-	AppRoutingModule,		// Contains routes: MUST be after all the other feature modules for routing to work properly because it contains the wildcard route
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavMenuComponent,
+		FetchDataComponent,
+	],
+	imports: [
+		BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+		HttpClientModule,
+		FormsModule,
+		MaterialModule,
+		ComicsModule,
+		VendorsModule,
+		BrowserAnimationsModule,
+		AppRoutingModule,		// Contains routes: MUST be after all the other feature modules for routing to work properly because it contains the wildcard route
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ClientApp/src/app/vendors/vendors.module.ts
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////////////
+// Vendors Module
+// Search, Create, Edit, Delete Vendors
+///////////////////////////////////////////////////////////////////////////////////////
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class VendorsModule { }
